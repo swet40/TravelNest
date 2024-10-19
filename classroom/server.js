afc56f5fc61ca718f3a0045d69b16c7d348b2ebf
Delta student <swetapal101112@gmail.com>
2024-10-19 08:13:51 +0530

    server

--- classroom/server.js
+++ classroom/server.js
@@ -2,33 +2,35 @@ const express= require("express");
 const app = express();
 const users = require("./routes/user.js")
 const posts = require("./routes/post.js")
-const cookieParser = require("cookie-parser")
+const session = require("express-session");
 
-app.use(cookieParser("secretcode"));
+const sessionOptions = {
+    secret: "mysecretstring", resave: false, saveUninitialized: true,
+}
 
-app.get("/getsignedcookies",(req,res)=>{
-    res.cookie("made-in","china", {signed:true});
-    res.send("signed cookie sent");
+app.use(session(sessionOptions));
+
+app.get("/register",(req,res)=>{
+    let {name = "anonymous"} = req.query;
+    res.send(name);
 })
 
-app.get("/getcookies",(req,res) =>{
-    res.cookie("from","india");
-    res.cookie("greet","namaste");
-    res.send("sent you some cookies!");
+app.get("/hello",(req,res)=>{
+    res.send(`hello`);
 })
 
-app.get("/verify",(req,res) =>{
-    console.log(req.signedCookies);
-    res.send("verified");
-})
+// app.get("/reqcount", (req,res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} itmes.`);
+// })
 
-app.get("/", (req,res)=>{
-    let {name= "anonymous"} = req.cookies;
-    res.send(`Hi, ${name}`);
-})
-
-app.use('/users', users);
-app.use('/posts', posts);
+// app.get("/test", (req,res)=>{
+//     res.send("test successful");
+// })
 
 app.listen(3000, () =>{
     console.log("server is listening to port 3000");
